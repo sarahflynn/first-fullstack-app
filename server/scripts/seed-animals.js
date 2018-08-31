d1d6da228b64a912053ecafd5e53fdf245bb9257
Sarah Flynn <sarah.flynn@icloud.com>
2018-08-30 18:54:08 -0700

    end of lab time

--- server/scripts/seed-animals.js
+++ server/scripts/seed-animals.js
@@ -4,18 +4,19 @@ const animals = require('../data/cute-animals.json');
 Promise.all(
   animals.map(animal => {
     return client.query(`
-        INSERT INTO animals (name, type_id, age, dangerous, url
+        INSERT INTO animals (
+          type_id, name, age, dangerous, url
         )
         SELECT
-          id git 
+          id as type_id,
           $1 as name,
-          $3 as age,
-          $4 as dangerous,
-          $5 as url
+          $2 as age,
+          $3 as dangerous,
+          $4 as url
         FROM types
-        WHERE type = $2;
+        WHERE type = $5;
     `,
-    [animal.name, animal.type_id, animal.age, animal.dangerous, animal.url]
+    [animal.name, animal.age, animal.dangerous, animal.url, animal.type]
     );
   })
 )
